de835b9fb26b9286ca611a6ac44b1b4c1b96333e
codemathsz <mo284891@gmail.com>
2024-03-06 10:58:49 -0300

    configurer tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ['./src/renderer/**/*.{js,jsx,ts,tsx}'],
-  theme: {},
+  content: ['./src/renderer/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}', './src/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend:{
+      fontFamily:{
+        sans: ['Poppins',...defaultTheme.fontFamily.sans],
+        robotoMono: ['Roboto Mono', 'monospace'],
+      },
+      colors:{
+        primary: '#012030',
+        secondary: '#13678A',
+        blue: '#13678A',
+        'gray-150': '#F5F5F5',
+        'gray-250': '#EAEAEA',
+      }
+    }
+  },
   variants: {},
   plugins: [],
 };
