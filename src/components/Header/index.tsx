636acf9df2764951ebd38f53786e61d619f9f0bb
codemathsz <mo284891@gmail.com>
2024-03-14 18:12:27 -0300

    add navlink in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 export function Header(){
   return(
     <div className="w-full flex justify-between items-center gap-10 px-32 py-4 bg-white border-b border-b-[#EDEDED] shadow-md">
       <div className="w-full flex justify-start items-center">
         <nav className="flex gap-8 font-sans font-medium text-base text-primary">
-          <a href="#" className="border-b-2 border-b-transparent hover:border-b-2 hover:border-b-primary">Clientes</a>
-          <a href="#" className="border-b-2 border-b-transparent hover:border-b-2 hover:border-b-primary">Produtos</a>
+          <NavLink className="border-b-2 border-b-transparent hover:border-b-2 hover:border-b-primary" to="/">
+            Clientes
+          </NavLink>
+          <NavLink className="border-b-2 border-b-transparent hover:border-b-2 hover:border-b-primary" to="/Product">
+            Produtos
+          </NavLink>
         </nav>
       </div>
       <div className="w-full flex justify-end items-center">
